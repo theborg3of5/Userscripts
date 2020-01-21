09ee11cea58ef72b417cd83cd3c47c3e8c3f3cf1
Gavin Borg <theborg3of5@gmail.com>
2020-01-21 15:52:09 -0600

    Figured out how to get the user's includes/matches
    This will be useful for adding settings based on the individual site.

--- anchorHotkeys/anchorHotkeys.user.js
+++ anchorHotkeys/anchorHotkeys.user.js
@@ -1,17 +1,22 @@
 // ==UserScript==
 // @name         Anchor Hotkeys
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/theborg3of5/Userscripts/
 // @version      0.5
 // @description  Adds single-key hotkeys that jump to specific anchors on a page.
 // @author       Gavin Borg
 // ==/UserScript==
 
-// GDB TODO: update namespace before uploading anywhere
 // GDB TODO: make keys/anchor names configurable somewhere
 // GDB TODO: include warning about having to specify your own match/includes to make this work at all
 
 (function() {
     'use strict';
+
+    //alert(GM_info.script.options.override.use_matches[0]);
+    //GM_info.script.options.override.use_includes[i];
+    var sites = GM_info.script.options.override.use_matches;
+    sites.concat(GM_info.script.options.override.use_includes);
+    console.log(sites);
 
     // These are the keys and anchors (including leading #) that those keys should jump to.
     var anchorKeys = {
