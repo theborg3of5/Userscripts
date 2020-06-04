eb4e9bf3099a514391f1f7399b4b6ebcc8f608ff
Gavin Borg <theborg3of5@gmail.com>
2020-06-04 13:43:19 -0500

    New script: remove target attribute from specified links so they open in the current page instead of a new window.

---
+++ fixNewWindowLinks/fixNewWindowLinks.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Fix New-Window Links (Remove target Attribute)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Remove the target attribute from the links matching the given string.
+// @author       Gavin Borg
+// @match        
+// @grant        none
+// ==/UserScript==
+
+var linkQueryStrings = [""];
+
+(function() {
+    'use strict';
+
+    fixLinks();
+})();
+
+function fixLinks() {
+    for(var i = 0; i < linkQueryStrings.length; i++) {
+        fixLinksWithQuery(linkQueryStrings[i]);
+    }
+}
+
+function fixLinksWithQuery(queryString) {
+    if (document.querySelector(queryString)) { // Make sure the links we want to fix exist, in case they show up via AJAX a little later than page load.
+        document.querySelectorAll(queryString).forEach(link => link.removeAttribute("target"));
+    } else {
+        setTimeout(fixLinksWithQuery.bind(null, queryString), 0);
+    }
+}
