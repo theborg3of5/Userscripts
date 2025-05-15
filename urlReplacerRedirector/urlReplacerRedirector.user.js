6df63fa80a9b15635aae4a7e3251e924938a8f62
Gavin Borg <theborg3of5@gmail.com>
2025-05-14 21:35:07 -0500

    Got initial config page working
    Just for loading/saving values so far.

--- urlReplacerRedirector/urlReplacerRedirector.user.js
+++ urlReplacerRedirector/urlReplacerRedirector.user.js
@@ -4,31 +4,42 @@
 // @version      1.1
 // @description  Redirect specific sites by replacing part of the URL.
 // @author       Gavin Borg
-// @require      https://greasyfork.org/scripts/28536-gm-config/code/GM_config.js?version=184529
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @match        https://greasyfork.org/en/scripts/403100-url-replacer-redirector
 // @grant        GM_registerMenuCommand
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM.getValue
+// @grant        GM.setValue
 // ==/UserScript==
 
+// GM_* are for legacy Greasemonkey, GM. is for modern (GM 4+ sounds like)
+
+//gdbtodo do I need GM.registerMenuCommand too?
+
 //gdbtodo figure out a conversion from old to new settings style?
-//gdbtodo are the GM_getValue/GM_setValue grants above still needed? Might be a dependency of GM_config, or GM_config might replace that?
+//         - Should basically be able to load the old settings, add them to the GM_config fields, and save it, right? Or do I need a new reload or something then?
 //gdbtodo figure out an escape hatch to remove redirect settings for a specific site
 //           Maybe a menu option? But that would require the target site to be on the include/match list too?
+//gdbtodo consider defaulting prefix to matched site?
 
 //gdbtodo new idea for config handling: use the greasy fork page and/or the github page to host the config
 //         - That way there's always a reliable spot they can go to
-//         - Also make it available on all includes/matches>
-//            - Maybe filter down to just the current site in that case, with a notice + link to the page(s) where they could see all sites together?
-//            - Make this super clear in the updated README
-//         - Include a "delete redirects for this site" button at the site level (regardless of whether it's filtered)
+//         - Just make the config available via menu on any included/matched site
+//         - Include a "delete redirects for this site" button at the site level
 
-var config = GM_config;
+// var config = GM_config;
+var Config;
 
 (function() {
     'use strict';
 
-    initConfig(getMatchingSite());
+    const site = getMatchingSite();
+    console.log("Matching site: " + site); // gdbtodo probably won't need this eventually, just keeping in for debugging
+    initConfig();
+    
+    return;
+
 
     // Initial creation of settings structure if it doesn't exist
     if(!GM_getValue("replaceTheseStrings")) {
@@ -77,43 +88,66 @@ function getMatchingSite() {
     }
 }
 
-function initConfig(site) {
-    var siteClean = cleanSite(site);
+function initConfig() {
+    // var siteClean = cleanSite(site);
+    const configId = "URLReplacerRedirectorConfig";
 
-    // // Build the fields for each of the available hotkeys
-    // var fields = {};
-    // for (var i = 1; i <= maxNumHotkeys; i++) {
-    //     fields[keyField(i)] = {
-    //         label: "Key(s) to press:",
-    //         title: "The single key(s) to press to jump to this anchor/text. To have multiple keys jump to the same place, separate keys with a space (i.e. \"a r\" for both \"a\" and \"r\" keys ).",
-    //         type: "text",
-    //         labelPos: "above"
-    //     };
-    //     fields[anchorNameField(i)] = {
-    //         label: "Anchor name:",
-    //         title: "The name or id of the anchor to jump to",
-    //         type: "text"
-    //     };
-    //     fields[textField(i)] = {
-    //         label: "Text to jump to:",
-    //         title: "We'll jump to the first instance of this text on the page. Ignored if anchor name is specified.",
-    //         type: "text"
-    //     };
-    // }
+    
+    // Build config fields for each available site
+    var fields = {};
+    var floatIDs = [];
+    var sites = GM_info.script.options.override.use_matches;
+    sites.concat(GM_info.script.options.override.use_includes);
+    for (var site of sites)
+    {
+        fields[fieldSection(site)] = {
+            section: site,
+            type: "hidden", // Using a hidden field just to create the section header (could technically go on the prefix field below)
+        }
+        fields[fieldPrefix(site)] = {
+            label: "Prefix",
+            labelPos: "left",
+            type: "text",
+            title: "gdbdoc",
+        }
+        fields[fieldSuffix(site)] = {
+            label: "Suffix",
+            labelPos: "left",
+            type: "text",
+            title: "gdbdoc",
+        }
+        fields[fieldTargetStrings(site)] = {
+            label: "Strings to replace",
+            labelPos: "above",
+            type: "textarea",
+            title: "gdbdoc",
+        }
+        fields[fieldReplacementStrings(site)] = {
+            label: "Replace with strings",
+            labelPos: "above",
+            type: "textarea",
+            title: "gdbdoc",
+        }
+    }
 
-    config.init({
-        id: "URLReplacerRedirectorConfig-" + siteClean,
-        title: "Redirector Config for: " + site,
-        fields: fields
+    // Float the target strings fields to the left so that they can line up with their corresponding replacements
+    // const styles = floatIDs.join(", ") + " { float: left; }";
+    const styles = "div[id*=" + fieldTargetStrings("") + "] { float: left; }";
+
+    Config = new GM_config({
+        id: configId,
+        title: "URL Replacer/Redirector Config",
+        fields: fields,
+        css: styles,
     });
 
     // Add a menu item to the menu to launch the config
-    GM_registerMenuCommand('Configure hotkeys for this site', () => {
-        config.open();
+    GM_registerMenuCommand('Configure redirect sites and settings', () => {
+        Config.open();
     })
 }
 
-function cleanSite(site) {
+function cleanSite(site) { // gdbtodo is this worth keeping?
     return site.replace(/[\*/:\?\.]/g, ""); // Drop */:?. characters from site for use in ID
 }
 
@@ -125,11 +159,28 @@ function escapeRegex(string) { // gdbtodo only keep this if I actually end up ne
 // gdbtodo example of how to get values from the config
 // function getAnchorNameForKey(key) {
 //     for (var i = 1; i <= maxNumHotkeys; i++) {
-//         var keyAry = config.get(keyField(i).split(" "));
+//         var keyAry = Config.get(keyField(i).split(" "));
 //         if (keyAry.includes(key)) {
-//             return config.get(anchorNameField(i));
+//             return Config.get(anchorNameField(i));
 //         }
 //     }
 
 //     return "";
 // }
+
+//gdbtodo put these in a region probably
+function fieldSection(site) {
+    return "Section_" + site;
+}
+function fieldPrefix(site) {
+    return "Prefix_" + site;
+}
+function fieldSuffix(site) {
+    return "Suffix_" + site;
+}
+function fieldTargetStrings(site) {
+    return "TargetString_" + site;
+}
+function fieldReplacementStrings(site) {
+    return "ReplacementString_" + site;
+}
